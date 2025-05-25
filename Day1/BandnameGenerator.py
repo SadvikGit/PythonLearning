172776e28c7f9b75014baf963df2619a19ffa766
SadvikGit <sadvikbeerala4572@gmail.com>
2025-05-25 12:05:05 +0530

    BandnameGenerator

--- Day1/BandnameGenerator.py
+++ Day1/BandnameGenerator.py
@@ -1,4 +1,4 @@
 print("**Welcome to My Brand_name Generator**")
 name=input("Enter Your name:")
 pet_name=input("Enter Your Pet_name:")
-print("Your Bandname is "+ ""name+pet_name"")
+print("Your Bandname is " + name+pet_name)
