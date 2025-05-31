2c3fb7cb208cbd34df946fcca0cbf2f1ab65834d
SadvikGit <sadvikbeerala4572@gmail.com>
2025-05-31 23:42:59 +0530

    Password_Generator

---
+++ DAY5/Highest_score.py
@@ -0,0 +1,6 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+maxx =0
+for i in student_scores :
+    if  i >= maxx:
+        maxx=i
+print(f"The Highest Score Among Given list is {maxx}.")
